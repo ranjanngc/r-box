511c7d0505ef37a751d5343c2cbfccc2e822e739
Ranjan Kumar <ranjan.kumar@amsoftware.com>
2022-07-27 19:12:51 +0530

    initial work

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# r-div
+# r-box
+
+r-box is an initiative to create a web component to host your HTML and add reactivity to it.
+
+# Uses
+```HTML
+<r-box :bind="dataObject">
+    <!-- HTML Template-->
+</r-box>
+
+<!--data object to bind with r-box-->
+<script>
+    var dataObject = { };
+</script>
+
+<!--add r-box script-->
+<script src="src/r-box.js" defer></script>
+```
+
+# Directive
+r-box has very few directives to add reactivity to HTML
+|directive|description|
+|---------|-----------|
+|data-*| To add reactivity to element attribute use `data-*` directive. For example to add a dynamic class you can use `data-class=methodToReturnClass`| 
+|react-on| string representation of list of properties on change of which DOM should adopt the change|
+
+# Interpolation
+user `${yourVariableName}` to interpolate the `yourVariableName` in template. E.g.
+```
+<h1>${title}</h1>
+```
+Limitation - composite variable interpolation does not work. In case of complex interpolation use function. E.g.
+```
+<h1>${yourFunctionName}</h1> // yourFunctionName: () => {return var1 + var2 + var3}
+```
+
+# data-*
+```
+<input type="text" data-value="yourVariableName">
+```
+
+# Example
+```HTML
+<r-box :bind="dataObject">
+
+    <label>${label}</label>
+    <input type="text" data-value="time">
+</r-box>
+
+<script>
+    var dataObject = {
+
+        label: 'Time',
+        time: ''
+    };
+
+    window.setInterval(() => {
+        dataObject.time = (new DateTime()).toLocalTime();
+    }, 1000);
+    
+</script>
+```
+![sample](./docs/assets/sample01.gif)
---
+++ src/r-box.js
@@ -0,0 +1,136 @@
+class RBox extends HTMLElement {
+
+    _select     = null;
+    _bind       = null;
+    _items      = [{key: '-1', value: 'empty'}];
+    _data       = null;
+    _bindMap    = {};
+
+    constructor() {
+
+      super();
+
+      this._bind        = this.getAttribute(":bind");
+      this._select      = document.createElement('select');
+      this.bindObject   = window[this._bind];
+      this._data        = {...this.bindObject, map: {}, interpolation: {}};
+      
+      this.parseChild(this);
+
+      this.attachProxy();
+    }
+    
+    parseChild = (node) => {
+
+        if(node.childNodes){
+
+            this.parseNodes(node.childNodes);
+        }
+    }
+
+    parseNodes = (nodes) => {
+
+        nodes.forEach(node => {
+
+            for(let data in node.dataset){
+
+                this.initializeNode(node, data);
+            }
+    
+            this.applyInterpolation(node);
+            this.parseChild(node);
+        });
+    }
+
+    initializeNode(node, attr) {
+
+        const prop = node.dataset[attr];
+        
+        
+        const fn = {el: node, attr: attr, bind: prop};
+        
+        const reactOn = JSON.parse(node.getAttribute('react-on') ?? "[]");
+        reactOn.push(prop)
+        reactOn.forEach(p => {
+            this._data.map[p] = this._data.map[prop] || [];
+            this._data.map[p].push(fn);
+        })
+        node.setAttribute(attr, (typeof(this._data[prop]) === 'function' ? this._data[prop](): this._data[prop]));
+        this.attachEvents(node, attr, prop);
+    };
+
+    applyInterpolation = (node) => {
+
+        if(node.textContent.indexOf('${') > -1) {
+
+            const prop = node.textContent.replace('${', '').replace('}','').replace(' ', '').trim();
+            
+            const reactOn = JSON.parse(node.getAttribute('react-on') ?? "[]");
+            reactOn.push(prop)
+
+            const ipl = {el: node, text: () => {
+
+                if(typeof(this._data[prop]) === 'function'){return this._data[prop]();}
+                return `${this._data[prop]}`
+            }};
+
+            reactOn.forEach(p => {
+                this._data.interpolation[p] = this._data.interpolation[prop] || [];
+                this._data.interpolation[p].push(ipl);
+            });
+            
+
+            node.innerHTML = ipl.text();
+        }
+    }
+
+    attachProxy = () => {
+
+        window[this._bind] = new Proxy(this._data, {
+
+            set(target, prop, value) {
+
+                target[prop] = value;
+
+                target.map[prop] && target.map[prop].forEach((child)=>{
+                    const attrValue = typeof(target[child.bind]) === 'function' ? target[child.bind]() : target[child.bind];
+                    child.el.setAttribute(child.attr, attrValue);
+
+                    if(typeof(attrValue) === 'boolean') {
+        
+                        attrValue ? 
+                            child.el.setAttribute(child.attr, 'true') : 
+                            child.el.removeAttribute(child.attr);
+                    }
+
+                    if(child.el[child.attr]){
+
+                        child.el[child.attr] = attrValue;
+                    }
+                });
+                
+                if(target.interpolation[prop]) {
+                    target.interpolation[prop].forEach((child)=>{
+                        
+                        child.el.innerHTML = child.text(); 
+                    })
+                }
+    
+              return true;
+            }
+        })
+    }
+
+    attachEvents = (child, attr, prop)=> {
+
+        if(['value', 'checked'].indexOf(attr) !== -1) {
+        
+            child.addEventListener('input', (ev) => {
+
+                window[this._bind][prop] = (typeof(window[this._bind][prop]) === 'boolean') ? ev.target.checked : ev.target.value;
+            })
+        }
+    }
+};
+  
+customElements.define("r-box", RBox);
